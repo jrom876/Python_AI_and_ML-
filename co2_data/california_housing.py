4b38f19c333631277e0e07810cc204106028f60f
jrom876 <jrom876@gmail.com>
2024-12-22 07:51:49 -0700

    Update california_housing.py

--- co2_data/california_housing.py
+++ co2_data/california_housing.py
@@ -1,18 +1,13 @@
 ## https://stackoverflow.com/questions/53184361/how-to-load-sklearn-datasets-manually
+### https://scikit-learn.org/stable/api/sklearn.datasets.html
 
 import pandas as pd
 from sklearn.datasets import fetch_california_housing
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import GradientBoostingRegressor
-from sklearn.ensemble import RandomForestRegressor
 
-### https://scikit-learn.org/stable/api/sklearn.datasets.html
 def show_calif_hous_data(num):	
 	data = fetch_california_housing()
-	calf_hous_df = pd.DataFrame(data= data.data, columns=data.feature_names)    
-	print(calf_hous_df.head(num))
-
+	calif_hous_df = pd.DataFrame(data= data.data, columns=data.feature_names)    
+	print(calif_hous_df.head(num))
 
 if __name__ == "__main__":
 	
