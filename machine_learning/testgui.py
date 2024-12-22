347ae9ad0164f22c3ab882dbe1d2907a81bbc990
jrom876 <jrom876@gmail.com>
2024-12-22 10:19:52 -0700

    Adding display options to testgui.py

--- machine_learning/testgui.py
+++ machine_learning/testgui.py
@@ -4,7 +4,10 @@
 import matplotlib
 import matplotlib.pyplot as plt
 
+# ~ matplotlib.use('Agg')
 matplotlib.use('QtAgg')
+# ~ matplotlib.use('TkAgg')
+# ~ matplotlib.use('SVG')
 
 print(matplotlib.get_backend())
 plt.plot((1, 4, 6))
